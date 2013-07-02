7a39192fc2fe3a96adb768600ffea6c2d8c11159
Thuy Dinh Xuan <thuydx@zendgroup.vn>
2013-07-02 21:41:23 +0700

    Clear document
    Signed-off-by: Thuy Dinh Xuan <thuydx@zendgroup.vn>

--- vendors/ZendDeveloperTools/src/ZendDeveloperTools/Exception/CollectorException.php
+++ vendors/ZendDeveloperTools/src/ZendDeveloperTools/Exception/CollectorException.php
@@ -2,7 +2,6 @@
 /**
  * Zend Developer Tools for Zend Framework (http://framework.zend.com/)
  *
- * @link       http://github.com/zendframework/ZendDeveloperTools for the canonical source repository
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd New BSD License
  * @package    ZendDeveloperTools
