a4d71035d894d6bdbd5649658d3f5469e2cd9332
Thuy Dinh Xuan <thuydx@zendgroup.vn>
2014-03-05 03:51:32 +0700

    change vendor
    Signed-off-by: Thuy Dinh Xuan <thuydx@zendgroup.vn>

--- public/index.php
+++ public/index.php
@@ -6,7 +6,17 @@
 chdir(dirname(__DIR__));
 
 // Setup autoloading
-include 'vendors/Autoload/definition.php';
+include 'vendor/definition.php';
+
+
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+	return false;
+}
+
+// Setup autoloading
 require 'init_autoloader.php';
+
 // Run the application!
 Zend\Mvc\Application::init(require 'configs/application.config.php')->run();
