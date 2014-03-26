5e0daf9a184f1618c8728234ddba1666f0d28c58
Thuy Dinh Xuan <thuydx@zendgroup.vn>
2014-03-26 20:31:43 +0700

    remove not used module user

--- frontend/Users/Module.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require_once __DIR__ . '/src/Users/Module.php';
--- frontend/Users/autoload_classmap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return array (
-	'Users\Controller\IndexController' 					=> __DIR__ . '/src/Users/Controller/IndexController.php',
-);
