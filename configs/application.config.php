eb5014c4cf728f4da7b962defacdb923be0ca7bf
Thuy Dinh Xuan <thuydx@zendgroup.vn>
2014-03-27 00:57:31 +0700

    prepare module will be code

--- configs/application.config.php
+++ configs/application.config.php
@@ -1,19 +1,31 @@
 <?php
+$env = getenv('APP_ENV') ? : 'production';
+$baseDir = dirname(dirname(__FILE__));
+$cacheDir = $baseDir . '/data/cache';
+
+// Use the $env value to determine which modules to load
+$modules = array(
+    'AdminCP',
+    'Content',
+    'Front',
+    'Gallery',
+    'Blogs',
+    'Category',
+    'Themes',
+    'System',
+    'Statistic',
+    'DbProfiler',
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'SyntaxHighlight',
+);
+if ($env == 'development') {
+    $modules[] = 'ZendDeveloperTools';
+}
+
 return array(
     // This should be an array of module namespaces used in the application.
-    'modules' => array(
-        'AdminCP',
-        'Content',
-        'Front',
-        'Users',
-        'DbProfiler',
-        'DoctrineModule',
-        'DoctrineORMModule',
-        'ZendDeveloperTools',
-        'SyntaxHighlight',
-//         'Test'
-    ),
-
+    'modules' => $modules,
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
         // This should be an array of paths in which modules reside.
@@ -26,50 +38,42 @@ return array(
             './modules',
             './vendor',
         ),
-
         // An array of paths from which to glob configuration files after
         // modules are loaded. These effectively overide configuration
         // provided by modules themselves. Paths may use GLOB_BRACE notation.
         'config_glob_paths' => array(
             'configs/autoload/{,*.}{global,local}.php',
         ),
-
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        //'config_cache_enabled' => $booleanValue,
-
+        'config_cache_enabled' => ($env == 'production'),
         // The key used to create the configuration cache file name.
-        //'config_cache_key' => $stringKey,
-
+        'config_cache_key' => 'zgs_cache',
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        //'module_map_cache_enabled' => $booleanValue,
-
+        'module_map_cache_enabled' => ($env == 'production'),
         // The key used to create the class map cache file name.
-        //'module_map_cache_key' => $stringKey,
-
+        'module_map_cache_key' => 'zgs_module_cache',
         // The path in which to cache merged configuration.
-        //'cache_dir' => $stringPath,
-
+        'cache_dir' => $cacheDir,
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
-        // 'check_dependencies' => true,
+        'check_dependencies' => ($env != 'production'),
     ),
+// Used to create an own service manager. May contain one or more child arrays.
+//'service_listener_options' => array(
+//     array(
+//         'service_manager' => $stringServiceManagerName,
+//         'config_key'      => $stringConfigKey,
+//         'interface'       => $stringOptionalInterface,
+//         'method'          => $stringRequiredMethodName,
+//     ),
+// )
 
-    // Used to create an own service manager. May contain one or more child arrays.
-    //'service_listener_options' => array(
-    //     array(
-    //         'service_manager' => $stringServiceManagerName,
-    //         'config_key'      => $stringConfigKey,
-    //         'interface'       => $stringOptionalInterface,
-    //         'method'          => $stringRequiredMethodName,
-    //     ),
-    // )
-
-   // Initial configuration with which to seed the ServiceManager.
-   // Should be compatible with Zend\ServiceManager\Config.
-   // 'service_manager' => array(),
+// Initial configuration with which to seed the ServiceManager.
+// Should be compatible with Zend\ServiceManager\Config.
+// 'service_manager' => array(),
 );
