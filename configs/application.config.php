857b815d0c10a092dd1100104dc2a99746ba275a
Thuy Dinh Xuan <thuydx@zendgroup.vn>
2014-04-06 08:57:48 +0700

    Resolve conflic
    Signed-off-by: Thuy Dinh Xuan <thuydx@zendgroup.vn>

--- configs/application.config.php
+++ configs/application.config.php
@@ -1,16 +1,11 @@
 <?php
-<<<<<<< HEAD
-$env = getenv('APP_ENV') ? : 'production';
-=======
 
 $env = getenv('APP_ENV') ?: 'production';
->>>>>>> upstream/master
 $baseDir = dirname(dirname(__FILE__));
 $cacheDir = $baseDir . '/data/cache';
 
 // Use the $env value to determine which modules to load
 $modules = array(
-<<<<<<< HEAD
     'AdminCP',
     'Content',
     'Front',
@@ -25,17 +20,6 @@ $modules = array(
     'DoctrineORMModule',
     'SyntaxHighlight',
 );
-=======
-        'AdminCP',
-        'Content',
-        'Front',
-        'Users',
-        'DbProfiler',
-        'DoctrineModule',
-        'DoctrineORMModule',
-        'SyntaxHighlight',
-    );
->>>>>>> upstream/master
 if ($env == 'development') {
     $modules[] = 'ZendDeveloperTools';
 }
@@ -43,10 +27,6 @@ if ($env == 'development') {
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => $modules,
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/master
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
         // This should be an array of paths in which modules reside.
@@ -69,25 +49,18 @@ return array(
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
         'config_cache_enabled' => ($env == 'production'),
-<<<<<<< HEAD
-        // The key used to create the configuration cache file name.
-        'config_cache_key' => 'zgs_cache',
-=======
 
         // The key used to create the configuration cache file name.
         'config_cache_key' => 'zgs_cache',
 
->>>>>>> upstream/master
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
         'module_map_cache_enabled' => ($env == 'production'),
-<<<<<<< HEAD
         // The key used to create the class map cache file name.
         'module_map_cache_key' => 'zgs_module_cache',
         // The path in which to cache merged configuration.
         'cache_dir' => $cacheDir,
-=======
 
         // The key used to create the class map cache file name.
         'module_map_cache_key' => 'zgs_module_cache',
@@ -95,7 +68,6 @@ return array(
         // The path in which to cache merged configuration.
         'cache_dir' => $cacheDir,
 
->>>>>>> upstream/master
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
